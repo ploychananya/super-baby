048e5010a96fcfca2d8f70e252139cb3633fb6f9
ploychananya <ploy_youngmodulus@hotmail.com>
2017-10-05 23:56:10 +0700

    can_move_baby

--- super_baby.py
+++ super_baby.py
@@ -1,8 +1,13 @@
 import arcade
-import arcade.key 
+import arcade.key
+
 SCREEN_WIDTH = 1280
 SCREEN_HEIGHT = 720
 SCALE = 0.23
+I=0
+T=0
+Insert_key=False
+
  
 class SpaceGameWindow(arcade.Window):
     def __init__(self, width, height):   # justify shape of obj
@@ -11,21 +16,55 @@ class SpaceGameWindow(arcade.Window):
         arcade.set_background_color(arcade.color.WHITE)
 
         self.baby = arcade.Sprite('character/babyfly1.png',SCALE)
-        self.baby.set_position(250,360)  # baby w = 150 ,h=?
+        self.baby.set_position(250,300)  # baby w = 150 ,h=?
 
-        self.block =arcade.Sprite('character/block1.png',SCALE)
-        self.block.set_position(92,660) # block w=92 , h =60
- 
+
+        self.block =arcade.Sprite('character/fixblock.png',1)
+        self.block.set_position(640,420) # block w=92 , h =60
+
+        self.blood =arcade.Sprite('character/fixblood.png',1)
+        self.blood.set_position(640,355) # block w=92 , h =60
+   
+    def update(self, delta):
+    
+        global I,Insert_key,T
+        
+        
+    
+        if(Insert_key):
+            I=0
+            self.baby.set_position(self.baby.center_x, self.baby.center_y+(T*T))
+            T-=0.5
+            if(T<1):
+                Insert_key=False
+            
+         
+        elif(not Insert_key):
+            self.baby.set_position(self.baby.center_x, self.baby.center_y-(I*I))
+            I+=0.07
+            T=6
+            print(I)
 
     def on_draw(self):
         arcade.start_render()
+        #self.set_update_rate(20)
         self.baby.draw()
         self.block.draw()
-
+        self.blood.draw()
+   
     def on_key_press(self, key, key_modifiers):
-        self.baby.on_key_press(key, key_modifiers)
-        self.baby.set_position(250,self.baby.y+30)
+        global Insert_key
+        if key == arcade.key.UP:
+            #self.on_key_press(key, key_modifiers)
+            print("on key Up")
+            Insert_key=True
+            #global I
+     
+        
             
+           # print("ployloveJJ")
+           # self.baby.set_position(self.baby.center_x, self.baby.center_y+150)
+            #I=0        
  
 if __name__ == '__main__':
     window = SpaceGameWindow(SCREEN_WIDTH, SCREEN_HEIGHT)
