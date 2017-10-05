1b7a6716e10dccafe01eb99009421b6008c084d1
ploychananya <ploy_youngmodulus@hotmail.com>
2017-10-06 00:23:26 +0700

    add_all_obj

--- super_baby.py
+++ super_baby.py
@@ -18,18 +18,38 @@ class SpaceGameWindow(arcade.Window):
         self.baby = arcade.Sprite('character/babyfly1.png',SCALE)
         self.baby.set_position(250,300)  # baby w = 150 ,h=?
 
-
         self.block =arcade.Sprite('character/fixblock.png',1)
         self.block.set_position(640,420) # block w=92 , h =60
 
         self.blood =arcade.Sprite('character/fixblood.png',1)
-        self.blood.set_position(640,355) # block w=92 , h =60
+        self.blood.set_position(640,355) 
+
+
+        self.hok =arcade.Sprite('character/hok.png',0.35)
+        self.hok.set_position(555,150) 
+
+        self.hok_move =arcade.Sprite('character/hok_move.png',0.3)
+        self.hok_move.set_position(850,180)
+
+        self.kill_block1 =arcade.Sprite('character/killblock1.png',SCALE)
+        self.kill_block1.set_position(990,538) 
+
+        self.kill_block2 =arcade.Sprite('character/killblock2.png',SCALE)
+        self.kill_block2.set_position(300,538) 
+
+        self.bomb =arcade.Sprite('character/bomb.png',SCALE)
+        self.bomb.set_position(680,560) 
+        
+        self.eye =arcade.Sprite('character/eye.png',SCALE)
+        self.eye.set_position(1200,450)
+
+        self.ghost =arcade.Sprite('character/ghost.png',SCALE)
+        self.ghost.set_position(1200,230)
+
    
     def update(self, delta):
     
         global I,Insert_key,T
-        
-        
     
         if(Insert_key):
             I=0
@@ -51,6 +71,13 @@ class SpaceGameWindow(arcade.Window):
         self.baby.draw()
         self.block.draw()
         self.blood.draw()
+        self.hok.draw()
+        self.hok_move.draw()
+        self.kill_block1.draw()
+        self.kill_block2.draw()
+        self.bomb.draw()
+        self.eye.draw()
+        self.ghost.draw()
    
     def on_key_press(self, key, key_modifiers):
         global Insert_key
